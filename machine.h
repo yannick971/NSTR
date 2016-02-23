41d26cd650c85c06a85a7a230b634ea4279cb184
jenni13 <jennifer.manfredo@gmail.com>
2016-02-23 11:33:11 +0100

    aaaaaaaaaa

---
+++ machine.h
@@ -0,0 +1,5 @@
+#ifndef
+
+void *Th_Machine();
+
+#endif
---
+++ superviseur.h
@@ -0,0 +1,7 @@
+#ifndef SUPERVISEUR
+
+void *Th_Dialogue();
+void fonc_evenementielle_USER1 (list l);
+void fnc_evenementielle_SIGUSER2(list l);
+
+#endif
